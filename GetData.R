a0809dce06f4e3332a3fcdc63b677671c2040eb0
Rishi Sadhir <Rishi.Sadhir@gmail.com>
2015-03-07 19:00:21 -0600

    first commit

---
+++ GetData.R
@@ -0,0 +1,12 @@
+## First load all of the data
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+
+## Subset to proper dates
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates and times
+datetime <- paste(as.Date(data$Date), data$Time)
+data$DateTime <- as.POSIXct(datetime)
---
+++ plot2.R
@@ -0,0 +1,8 @@
+source("GetData.R")
+
+
+## Plot 2
+plot(data$Global_active_power~data$DateTime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
